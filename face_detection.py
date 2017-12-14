dcab5b4396cffb96a4260db1c18b1891c8829ef8
venkateshtata <venkateshtata9@gmail.com>
2017-12-14 11:46:46 +0530

    Capturing video stream

--- face_detection.py
+++ face_detection.py
@@ -11,6 +11,16 @@ def detect(gray, frame):
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+h]
         eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 3)
-         for (ex, ey, ew, eh) in eyes:
+        for (ex, ey, ew, eh) in eyes:
              cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 5)
     return frame
+video_capture = cv2.VideoCapture(0)
+while True:
+    _,frame = video_capture.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    canvas = detect(gray, frame)
+    cv2.imshow('video', canvas)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+video_capture.release()
+cv2.destroyAllWindows()
