c3b8847d8e92246dd96c846024d75050a82e260a
Venkatesh tata <venkateshtata@Venkateshs-MacBook-Pro.local>
2017-12-14 11:05:48 +0530

    drawing the rectangle around the face

--- face_detection.py
+++ face_detection.py
@@ -6,3 +6,5 @@ eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 def detect(gray, frame):
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
