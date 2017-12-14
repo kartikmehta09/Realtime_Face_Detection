d0931b20a8147878a9afa4ae9231f240ad70cc4d
Venkatesh tata <venkateshtata@Venkateshs-MacBook-Pro.local>
2017-12-14 10:59:28 +0530

    defining detection functon

--- face_detection.py
+++ face_detection.py
@@ -1,2 +1,8 @@
 import cv2
 
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') 
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+
+def detect(gray, frame):
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
