ce39af9fe9c64e7ac2df95baf83b4644c40ff717
Venkatesh tata <venkateshtata@Venkateshs-MacBook-Pro.local>
2017-12-14 11:14:49 +0530

    detecting eyes

--- face_detection.py
+++ face_detection.py
@@ -8,3 +8,6 @@ def detect(gray, frame):
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+h]
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 3)
